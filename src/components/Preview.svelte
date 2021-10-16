9bad356fd04952058d574df9da10d54562d75baf
sakkke <w32w64@gmail.com>
2021-10-16 10:41:18 +0900

    :zap: improve css

--- src/components/Preview.svelte
+++ src/components/Preview.svelte
@@ -4,11 +4,11 @@
 
 {#each rows as row}
     {#if row.length === 1}
-        <p class="break-words overflow-auto whitespace-pre-wrap">{row[0].text}</p>
+        <p class="break-words whitespace-pre-wrap">{row[0].text}</p>
     {:else}
         <ul class="flex">
             {#each row as column}
-                <li class="break-words flex-1 overflow-auto whitespace-pre-wrap">{column.text}</li>
+                <li class="break-words flex-1 min-w-0 whitespace-pre-wrap">{column.text}</li>
             {/each}
         </ul>
     {/if}
