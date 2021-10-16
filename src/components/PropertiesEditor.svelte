8afbe6c145b8161a8ab0660f6fa592e3faa88d5c
sakkke <w32w64@gmail.com>
2021-10-16 14:54:49 +0900

    :bug: fix broken layout for multiple `<label>`

--- src/components/PropertiesEditor.svelte
+++ src/components/PropertiesEditor.svelte
@@ -3,9 +3,11 @@
 </script>
 
 {#each Object.keys(v) as k}
-    {#if typeof v[k] === 'boolean'}
-        <label>{k} <input bind:checked={v[k]} type="checkbox"></label>
-    {:else if typeof v[k] === 'string'}
-        <label>{k} <textarea bind:value={v[k]} class="align-top border resize-none" cols="50" rows="3"></textarea></label>
-    {/if}
+    <div class="flex flex-col">
+        {#if typeof v[k] === 'boolean'}
+            <label>{k} <input bind:checked={v[k]} type="checkbox"></label>
+        {:else if typeof v[k] === 'string'}
+            <label>{k} <textarea bind:value={v[k]} class="align-top border resize-none" cols="50" rows="3"></textarea></label>
+        {/if}
+    </div>
 {/each}
